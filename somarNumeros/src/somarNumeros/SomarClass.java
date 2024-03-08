5593837825873dfbff4fdb0ab41c9d2c62534305
Luís Santiago <lamsantiago@sga.pucminas.br>
2024-03-08 00:32:20 -0300

    Integração Eclipse Github!

---
+++ somarNumeros/src/somarNumeros/SomarClass.java
@@ -0,0 +1,23 @@
+package somarNumeros;
+import java.util.Scanner;
+
+public class SomarClass {
+	public static void main(String[] args) {
+        //Declaração de variáveis
+		int num1, num2, soma;
+		Scanner sc = new Scanner(System.in);
+
+        //Leituras
+        System.out.println("Digite um número");
+		num1 = sc.nextInt();
+        System.out.println("Digite outro número");
+		num2 = sc.nextInt();
+
+        //Somar
+		soma = num1 + num2;
+
+        //Mostrar na tela
+		System.out.println(soma);
+		sc.close();
+}
+}
